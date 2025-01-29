4916eb0d6150d9cb1e3023a2b8beec00f3b1d3a1
teaxdev <FloresAlex6904@gmail.com>
2025-01-29 13:58:53 -0600

    add data type homework

--- 1/symbols.py
+++ 1/symbols.py
@@ -16,8 +16,8 @@ print("10/3 = ", 10/3)
 print("10 // 3 =", 10//3)
 print("10 % 3 = ", 10 % 3)
 
-print(10 > 2)
-print(10 < 2)
+print(10 > 4)
+print(10 < 4)
 print(10 == 2)
 print(10 != 2)
 
---
+++ 2/datatypeprc.py
@@ -0,0 +1,30 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+
+print("Literal:", 10)
+print('Literal:', 3214732)
+
+# Integer
+y = -20
+yy = -762
+print("Integer:", y)
+print('Integer:', yy)
+
+# Float
+z = 15.5
+zz = 3.14159
+print("Float:", z)
+print('Float:', zz)
+
+# String
+name = "John"
+last_name = "Doe"
+print("String:", name)
+print('String:', last_name)
+
+# Boolean
+is_active = True
+is_present = False
+print("Boolean:", is_active)
+print('Boolean:', is_present)
