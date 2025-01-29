9a65f7ec54cc621533f78a9c6c6f17d9dc100154
teaxdev <FloresAlex6904@gmail.com>
2025-01-29 13:17:45 -0600

    finish variables practice

--- 1/symbols.py
+++ 1/symbols.py
@@ -7,3 +7,20 @@ age = 53
 print(20)
 print(age)
 print('Alexander')
+
+print(4 + 4)
+print('Alex' + ' Flores')
+print('4' + '4')
+print("2 * 6 = ", 2 * 6)
+print("10/3 = ", 10/3)
+print("10 // 3 =", 10//3)
+print("10 % 3 = ", 10 % 3)
+
+print(10 > 2)
+print(10 < 2)
+print(10 == 2)
+print(10 != 2)
+
+for x in range(10):
+    if x % 2 == 0:
+        print(x)
