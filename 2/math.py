4bdf15b9b61188ecf69b79f257f621529873dec3
teaxdev <FloresAlex6904@gmail.com>
2025-02-02 14:33:41 -0600

    start math operations homework

---
+++ 2/math.py
@@ -0,0 +1,25 @@
+# Area of a rectangle practice
+
+length = 3
+width = 5
+area = length * width
+
+print(f"The area of the rectangle with a length of {length} and a width of {width} is {area}")
+
+length = 4
+width = 12
+area = length * width
+
+print(f"The area of the rectangle with a length of {length} and a width of {width} is {area}")
+
+length = 7
+width = 7
+perimeter = 2*(length + width)
+
+print(f"The perimeter of the rectangle with a length of {length} and a width of {width} is {perimeter}")
+
+length = 9
+width = 4
+perimeter = 2*(length + width)
+
+print(f"The perimeter of the rectangle with a length of {length} and a width of {width} is {perimeter}")
