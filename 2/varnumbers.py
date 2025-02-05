9a0672d9c3da9f5f581341a622dfbddf522933ff
teaxdev <FloresAlex6904@gmail.com>
2025-02-05 12:38:18 -0600

    finish variable numbers homework

--- 2/varnumbers.py
+++ 2/varnumbers.py
@@ -2,8 +2,36 @@
 
 kg = 5
 lb = kg * 2.20462
-print(lb)
+print(f"{kg} kilograms is {lb} pounds.")
 
 kg = 12
 lb = kg * 2.20462
-print(lb)
+print(f"{kg} kilograms is {lb} pounds.")
+
+inch = 10
+cm = inch * 2.54
+print(f"{inch} inches is {cm} centimeters.")
+
+cm = 13
+inch = cm * .3937008
+print(f"{cm} centimeters is {inch} inches.")
+
+mi = 5
+km = mi * 1.609344
+print(f"{mi} miles is {km} kilometers.")
+
+mi = 7
+km = mi * 1.609344
+print(f"{mi} miles is {km} kilometers.")
+
+km = 12
+mi = km * .6213712
+print(f"{km} kilometers is {mi} miles.")
+
+feet = 19
+yard = feet * .3333333
+print(f"{feet} feet is {yard} yards.")
+
+yard = 5
+feet = yard * 3
+print(f"{yard} yards is {feet} feet.")
