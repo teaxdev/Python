c0759ac150ac80022fa82a8931d8798d95cf5019
Alex <FloresAlex6904@gmail.com>
2025-02-10 13:00:56 -0600

    finish decisions homework

--- 3/decisions.py
+++ 3/decisions.py
@@ -1,6 +1,6 @@
 # Age homework & class practice
 
-temp = int(input("what is the temperature in fahrenheit?"))
+''' temp = int(input("what is the temperature in fahrenheit?"))
 
 if temp <= 0:
     print("sweater time!")
@@ -18,4 +18,29 @@ elif temp < 120:
     print("pool time")
 
 else:
-    print("doomsday!!!")
+    print("doomsday!!!") '''
+
+# Homework:
+
+age = int(input("what is your age in human years?"))
+
+if age <= 10:
+    print("what are you doing here")
+
+if age >= 15 <= 17:
+    print("student driver")
+
+if age >= 18:
+    print("adult driver")
+
+if age >= 18:
+    print("you can also vote")
+
+if age >= 21:
+    print("you can drink and buy alcoholic beverages!")
+
+if age <= 64:
+    print("ain't old enough for seniority status!")
+
+if age >= 65:
+    print("senior discount for you gramps")
