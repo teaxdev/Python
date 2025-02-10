7ed71fd78445b38d82925e8ca819e57623d6b583
Alex <FloresAlex6904@gmail.com>
2025-02-10 12:39:11 -0600

    add unit 3 directory and elif practice

---
+++ 3/decisions.py
@@ -0,0 +1,21 @@
+# Age homework & class practice
+
+temp = int(input("what is the temperature in fahrenheit?"))
+
+if temp <= 0:
+    print("sweater time!")
+
+elif temp < 30:
+    print("brr, it cold. time for shorts and t shirt")
+
+elif temp < 50:
+    print("Midwest Spring")
+
+elif temp < 70:
+    print("shirtless men running outside")
+
+elif temp < 120:
+    print("pool time")
+
+else:
+    print("doomsday!!!")
