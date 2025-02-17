9052876649ff432bb66cca77d68aaf257ec7fd06
Alex <FloresAlex6904@gmail.com>
2025-02-17 11:58:52 -0600

    finish elif homework

--- 3/elif.py
+++ 3/elif.py
@@ -1,2 +1,18 @@
 # else if python practice
 
+student_grade = int(input("What is your numeric grade?"))
+
+if student_grade < 60:
+    print("Your letter grade is: F")
+
+elif student_grade < 70:
+    print("Your letter grade is: D")
+
+elif student_grade < 80:
+    print("Your letter grade is: C")
+
+elif student_grade < 90:
+    print("Your letter grade is: B")
+
+else:
+    print("Your letter grade is: A")
