538cb4cbb015043c5d974c891b8e90072d80be24
Alex <FloresAlex6904@gmail.com>
2025-02-17 13:51:01 -0600

    add for_loop homework and comments on while homework

---
+++ 3/for_loop.py
@@ -0,0 +1,5 @@
+# 3.2 B for loop homework
+
+for i in range(1, 301):  # for loop for every number under 300
+    if i % 7 == 0:  # checks if the number is divisible by 7
+        print(i)
--- 3/while.py
+++ 3/while.py
@@ -1,16 +1,16 @@
 # 3.2 while loop homework
 
-count = 3
+count = 3  # sets count
 bottle = "bottles"
 
-while count > 0:
+while count > 0:  # loop that goes down to 0, and prints the amount of beer along with the song
     print(f"{count} " + bottle + " of beer on the wall")
     print(f"{count} " + bottle + " of beer")
     if count > 1:
         print("Take one down, pass it around")
     count -= 1
     if count == 1:
-        bottle = "bottle"
+        bottle = "bottle"  # changes the loop to have proper grammar when the count is 1
     if count > 0:
         print(f"{count} " + bottle + " of beer on the wall!")
         print("")
