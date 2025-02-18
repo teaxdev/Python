d2fb0b7e0fe49b7179600c72423366ffb323b74f
Alex <FloresAlex6904@gmail.com>
2025-02-18 12:59:32 -0600

    continue logic.py

--- 3/logic.py
+++ 3/logic.py
@@ -8,7 +8,7 @@ prompt = 6
 for i in range(1, prompt):
     if i / prompt != 1:
         continue
-    if 1 / prompt == 2:
-        print("A is a prime number")
-    else:
+    if 1 % prompt != 1:
         print("A is not a prime number")
+    else:
+        print("A is a prime number")
