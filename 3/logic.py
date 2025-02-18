e502d7f8cb0564c45f41e3c2cc2cd3fcb93a799a
Alex <FloresAlex6904@gmail.com>
2025-02-18 14:01:01 -0600

    fix prime number loop

--- 3/logic.py
+++ 3/logic.py
@@ -3,12 +3,12 @@
 # prompt = int(input("Enter an integer that will represent A: "))
 # prompt2 = int(input("Enter an integer that will represent B: "))
 
-prompt = 6
+prompt = 5
 
-for i in range(1, prompt):
-    if i / prompt != 1:
-        continue
-    if 1 % prompt != 1:
+for i in range(2, prompt):
+    if prompt % i == 0:
         print("A is not a prime number")
+        break
     else:
         print("A is a prime number")
+        break
