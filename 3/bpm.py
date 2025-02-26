e726ad2ee2c8558f37a136ba66cd6e31171ee773
Alex <FloresAlex6904@gmail.com>
2025-02-26 13:58:24 -0600

    finish bpm.py and add scope.py

--- 3/bpm.py
+++ 3/bpm.py
@@ -2,16 +2,27 @@
 # heart rate for each time slot. Create a multi level list called heart_rates to store in time slots
 # with corresponding heart rates. Then calculate heart rate and display it.
 
-time_slots = ["Morning", "Midday", "Afternoon", "Evening"]
-heart_rates = []
+time_slots = ["Morning", "Midday", "Afternoon",
+              "Evening"]  # defines the times of day
+heart_rates = []  # creates an empty list that will have a nested list
+rate_sum = []  # list that collects the heart rate to later get the average bpm
 grammar = "in the"
 
-for i in range(len(time_slots)):
-    if i == 1:
+# for loop that gets input on heart rate at different times of day.
+for time in range(len(time_slots)):
+    if time == 1:
         grammar = "at"
     else:
         grammar = "in the"
-    heart_rates.append(int(input(
-        f"What is your heart rate (in BPM) {grammar} {time_slots[i]}?: ")))
-    i += 1
-print(heart_rates)
+    rate = int(input(
+        f"What is your heart rate (in BPM) {grammar} {time_slots[time]}?: "))
+    heart_rates.append([time, rate])
+    rate_sum.append(rate)
+    time += 1
+
+# sums up the heart rate to calculate the average on line 25
+bpm_sum = sum(rate_sum)
+bpm_average = float(bpm_sum / len(time_slots))
+
+print(f"Your average heart rate today was: {bpm_average} BPM")
+# print(heart_rates)
