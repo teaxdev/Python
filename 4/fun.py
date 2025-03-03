0e311f2b47d3147d4418bfa3af99dade3e168949
Alex <FloresAlex6904@gmail.com>
2025-03-03 13:03:43 -0600

    add comments to fun.py

--- 4/fun.py
+++ 4/fun.py
@@ -1,7 +1,7 @@
 # 4.1 functions homework: define 2 functions, square and circle, where each function takes one parameter.
 # calculate the area of the square and the circle, and test whether they display th correct values.
 
-def square(side):
+def square(side):  # the square function calculates the area and displays it
     s_calc = side * side
     print(f"The area of the square is {s_calc} square units")
 
@@ -9,7 +9,7 @@ def square(side):
 square(4)
 
 
-def circle(radius):
+def circle(radius):  # the circle function calculates the area of the circle and displays it
     c_calc = 3.14 * radius * radius
     print(f"The area of the circle is {c_calc} square units")
 
