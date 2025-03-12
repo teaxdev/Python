141e8f8ac2124085649bb393003bef0a846dc6cb
Alex <FloresAlex6904@gmail.com>
2025-03-12 13:00:47 -0500

    finish factorials.py

--- 4/factorials.py
+++ 4/factorials.py
@@ -3,11 +3,13 @@
 # return n * factorial(n-1) if n > 1. prompt the user for a non-negative integer, then call and print factorial.
 
 def factorial(n):
-    n == 1
+    if n == 1 or n == 0:
+        return 1
     if n > 1:
         return n * factorial(n - 1)
+    else:
+        print("Please enter a non-negative number!")
 
 
 n = int(input("Please enter a non-negative number: "))
-factorial(n)
-print(n)
+print(factorial(n))
