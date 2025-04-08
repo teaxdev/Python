e746b6ec88454d349136ccef5d4b3fc82946fd97
teaxdev <FloresAlex6904@gmail.com>
2025-04-07 22:01:30 -0500

    finish error.py

--- PE2-2/error.py
+++ PE2-2/error.py
@@ -5,23 +5,54 @@ import random
 gen_random = random.randrange(1, 50)
 
 
-def intro():
-    print("Welcome to the number guessing game!")
-    print("What is your name?")
-    try:
-        name = input(str("Your name: "))
-    except ValueError:
-        print("I don't think you're a robot, please enter your name!")
+print("Welcome to the number guessing game!")
+print("What is your name?")
+name = input("Your name: ")
+print(f"Okay {name}, What difficulty would you like? (1 = Easy, 2 = Medium, 3 = Hard)")
+try:
+    challenge = int(input("Difficulty: "))
+    if challenge == 1:
+        diff = 10
+    elif challenge == 2:
+        diff = 7
+    else:
+        diff = 5
+except ValueError:
+    print("Please enter a valid difficulty! (1-3)")
+else: 
     print(f"Alright {name}, you're going to guess a number between 1-50!")
-    print("You get 5 tries!")
+finally:
+    print(f"You get {diff} tries!")
 
+
+
+
+def victory():
+    print(f"Congratulations! you guessed {gen_random}, and were correct!")
+
+def loss():
+    print(f"Bummer! Looks like you didn't guess {gen_random}!")
+    
 
 def main():
-    intro()
-    try:
-        input("Please set your initial guess!: ")
-    except ValueError:
-        print("Please enter a number from 1-50!")
+    tries = 0
+    while tries < diff:
+        try:
+            num = int(input("Please set your guess!: "))
+            if num > gen_random:
+                print("Lower!")
+                tries += 1
+            elif num < gen_random:
+                print("Higher!")
+                tries += 1
+            else:
+                victory()
+                break
+        except ValueError:
+            print("Please enter a valid number!")
+       
+    if tries > 7:
+         loss()
 
 
 main()
