31ba411f6ab73e39dad0b057b067c41aad324b19
teaxdev <FloresAlex6904@gmail.com>
2025-04-16 11:12:37 -0500

    start pet.py

---
+++ PE2-2/pet.py
@@ -0,0 +1,52 @@
+# 3.3 OOP Properties
+
+class Pet:
+
+    vet_name = "United Paws Veterinary Care"
+
+    def __init__(self, owner_first_name, owner_last_name, pet_id, pet_name, pet_type="dog"):
+        self.__owner_first_name = owner_first_name
+        self.__owner_last_name = owner_last_name
+        self.__pet_id = pet_id
+        self.__pet_name = pet_name
+        self.__pet_type = pet_type
+
+    def get_owner_first_name(self):
+        return self.__owner_first_name
+    
+    def get_owner_last_name(self):
+        return self.__owner_last_name
+        
+    def get_pet_id(self):
+        return self.__pet_id
+    
+    def get_pet_name(self):
+       return self.__pet_name
+    
+    def get_pet_type(self):
+       return self.__pet_type
+    
+    def set_owner_first_name(self, value):
+        self.__owner_first_name = value
+    
+    def set_owner_last_name(self, value):
+        self.__owner_last_name = value
+        
+    def set_pet_id(self, value):
+        self.__pet_id = value
+    
+    def set_pet_name(self, value):
+       self.__pet_name = value
+    
+    def set_pet_type(self, value):
+       self.__pet_type = value
+
+    def display_pet_info(self):
+       print("Pet and owner details:", vars(self))
+
+owner1 = Pet("James", "Smith", "12345", "Milo", "Dog")
+owner2 = Pet("Sam", "Mitchell", "12344", "Lana", "Cat")
+owner3 = Pet("Gavin", "Harders", "54321", "Covey", "Dog")
+
+owner1.display_pet_info()
+print(hasattr(Pet, "_Pet__owner_first_name"))
