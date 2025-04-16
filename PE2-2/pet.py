7838dc259f694c67084cdce92ac64a396fd5597e
Alex <FloresAlex6904@gmail.com>
2025-04-16 13:45:26 -0500

    finish pet.py

--- PE2-2/pet.py
+++ PE2-2/pet.py
@@ -4,7 +4,7 @@ class Pet:
 
     vet_name = "United Paws Veterinary Care"
 
-    def __init__(self, owner_first_name, owner_last_name, pet_id, pet_name, pet_type="dog"):
+    def __init__(self, owner_first_name, owner_last_name, pet_id, pet_name, pet_type="Dog"):
         self.__owner_first_name = owner_first_name
         self.__owner_last_name = owner_last_name
         self.__pet_id = pet_id
@@ -42,19 +42,33 @@ class Pet:
         self.__pet_type = value
 
     def display_pet_info(self):
-        print("Pet and owner details:", vars(self))
+        # print("Pet and owner details:", vars(self))
+        print(f"\n\nDisplay Owner Name: {self.__owner_first_name}")
+        print(f"Display Owner Last Name: {self.__owner_last_name}")
+        print(f"Display Pet ID: {self.__pet_id}")
+        print(f"Display Pet Name: {self.__pet_name}")
+        print(f"Display Pet Type: {self.__pet_type}")
 
-    def check_property(obj, property_name):
-        print(f"Has a {property_name} method: ", hasattr(obj, property_name))
+    print(f"Your veterinary: {vet_name}\n")
+
+
+def check_property(obj, property_name):
+    print(f"Has a {property_name} method: ", hasattr(obj, property_name))
 
 
 def main():
 
-    owner1 = Pet("James", "Smith", "12345", "Milo", "Dog")
+    owner1 = Pet("James", "Smith", "12345", "Milo")
     owner2 = Pet("Sam", "Mitchell", "12344", "Lana", "Cat")
-    owner3 = Pet("Gavin", "Harders", "54321", "Covey", "Dog")
+    owner3 = Pet("Gavin", "Harders", "54321", "Covey")
 
     check_property(owner1, "_Pet__owner_first_name")
+    check_property(owner2, "_Pet__owner_last_name")
+    check_property(owner3, "_Pet__pet_id")
+
+    owner1.display_pet_info()
+    owner2.display_pet_info()
+    owner3.display_pet_info()
 
 
-owner1.display_pet_info()
+main()
