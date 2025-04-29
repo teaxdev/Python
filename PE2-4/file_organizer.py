5b15e3b040394fcb935a9bd62531f618b63e45f7
teaxdev <FloresAlex6904@gmail.com>
2025-04-28 23:42:01 -0500

    finish file_organizer.py

---
+++ PE2-4/file_organizer.py
@@ -0,0 +1,10 @@
+import os
+
+def main():
+    os.mkdir("Python/PE2-4/MyFiles")
+    os.mkdir("Python/PE2-4/MyFiles/Docs")
+    os.mkdir("Python/PE2-4/MyFiles/Images")
+    os.mkdir("Python/PE2-4/MyFiles/Videos")
+
+
+main()
