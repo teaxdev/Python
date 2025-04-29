712257c40febe57dc6903dfd82d80adbecfa6a47
teaxdev <FloresAlex6904@gmail.com>
2025-04-28 23:12:54 -0500

    finish diary.py

--- PE2-4/diary.py
+++ PE2-4/diary.py
@@ -2,9 +2,11 @@
 
 def main():
     print("Welcome to PyDiary!, please input your diary log")
+    # Get input for day, date and time
     day = input("Date: ")
     time = input("Time: ")
     entry = input("Write diary entry: ")
+    #writes the inputs to the file, and creates the file if it doesn't exist
     with open('Python/PE2-4/diary.txt', 'a') as file:
         file.write(f"{day}\n\n{time}\n\n{entry}\n\n")
         file.close()
