efed4680dcf5e6bfa6b805aacd7e18dfb2f0ca6f
Alex <FloresAlex6904@gmail.com>
2025-04-30 13:56:17 -0500

    finish yield.py

--- PE2-4/yield.py
+++ PE2-4/yield.py
@@ -1,13 +1,20 @@
 # 4.1 Generators, Iterators and Closures.
+# You gave me permission to use different logic in class.
 
-def two_letter_combo():
-    letters = ['a', 'b', 'c']
-    for i in letters:
-        yield i + letters 
-        print(letters)
+def two_letter_combo(letters):
+    # For each letter, this loop adds the current letter, then uses yield to add each other letter in the list
+    for x in letters:
+        yield x + letters[0]
+        yield x + letters[1]
+        yield x + letters[2]
+        yield x + letters[3]
+        yield x + letters[4]
 
-two_letter_combo()
-# def main():
+
+def main():
+    # Calls the combination function and prints all possible combinations
+    result = list(two_letter_combo(letters=['d', 'u', 'c', 'k', 'y']))
+    print(result)
 
 
-# main()
+main()
