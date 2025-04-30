d0d9b05d04de895073140e5d55184afb89e90ff5
teaxdev <FloresAlex6904@gmail.com>
2025-04-30 11:08:17 -0500

    continue yield.py

--- PE2-4/yield.py
+++ PE2-4/yield.py
@@ -1,4 +1,13 @@
 # 4.1 Generators, Iterators and Closures.
 
-def two_letter_combo(letters):
-    letters = [x, y, z]
+def two_letter_combo():
+    letters = ['a', 'b', 'c']
+    for i in letters:
+        yield i + letters 
+        print(letters)
+
+two_letter_combo()
+# def main():
+
+
+# main()
