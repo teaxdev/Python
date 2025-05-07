1aba36ed3b22a73c33da53bc5fa74b7a97d098dc
Alex <FloresAlex6904@gmail.com>
2025-05-07 13:05:34 -0500

    finish cal.py

--- PE2-4/cal.py
+++ PE2-4/cal.py
@@ -2,6 +2,7 @@
 import calendar
 import datetime
 
+
 def main():
     datetime.datetime.now().year
     birth_month = int(input("What is your birth month? (1-12): "))
@@ -10,7 +11,12 @@ def main():
         main()
     else:
         print(calendar.month(2025, birth_month))
+        # cal = calendar.Calendar()
+        # for day in cal.itermonthdays(2024, birth_month):
+        #     print(day)
 
+# on a side note, naming your file calendar.py breaks every calendar module
+# I'm not sure if you ever mentioned that, if not it would be nice to add a note on the assignment page
 
 
 main()
