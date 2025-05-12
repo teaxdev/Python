a4f8a27dc185161f97895347fd7b705c836164da
Alex <FloresAlex6904@gmail.com>
2025-05-12 15:11:00 -0500

    continue crud.py

--- PE2-4/crud.py
+++ PE2-4/crud.py
@@ -7,7 +7,7 @@ def main_menu():
     try:
         print("\n                                         Menu              ")
         while True:
-            print("\nWelcome! You can reate new email entries, change email addresses, delete entries, or display entries")
+            print("\nWelcome! You can create new email entries, change email addresses, delete entries, or display entries")
             print("1. Create a new entry.")
             print("2. Display an entry by last name.")
             print("3. Update an existing entry. ")
@@ -45,28 +45,66 @@ def create():
     lname = input("Please enter the customer\'s last name:  ")
     phone = input("Please enter the customer\'s phone number:  ")
     email = input("Please enter the customer\'s email:  ")
-    entry = f"{fname}, {lname}, {phone}, {email}"
+    id = input("Please enter the customer\'s unique ID (5 numbers):  ")
+    entry = f"{fname}, {lname}, {phone}, {email}, {id}"
     print(entry)  # for test purposes
     customers.append(entry)
     print(customers)
     save(customers)
 
 
+def search():
+
+    try:
+        customers = check()
+
+        search_records = input("Enter the name of the customer: ")
+
+        for c in customers:
+            print(f"searching customers: {c}")
+            if search_records in c:
+                print("Customer Found\n")
+                c_index = customers.index(c)
+                return c, c_index
+        else:
+            print("Customer does not exist!")
+
+    except Exception as e:
+        print(f"Search: {e}")
+
+
 def read():
     # read a record - used by delete and update
     print("Read")
+    try:
+        # call SEARCH
+        found_customer, found_index = search()
+
+        print(
+            f"System Found Customer: {found_customer} at index: {found_index}")
+
+    except Exception as e:
+        print(f"READ: {e}")
+
+    main()
     # get current customer list  and get unique identifier
     # save as a variable
     # return a variable
 
 
 def update():
+    try:
+        print("apple")
+    except Exception as e:
+        print(f"UPDATE: {e}")
     # changes a record
     print("Update")
 
 
 def delete():
     # removes a record
+    search()
+
     print("Delete")
 
 
